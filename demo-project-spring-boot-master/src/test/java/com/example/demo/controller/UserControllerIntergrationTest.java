5b84cd6ed92911aaac119b366d252a4ce776da09
phieulong <phieulong97@gmail.com>
2020-01-11 13:05:03 +0700

    fix

--- demo-project-spring-boot-master/src/test/java/com/example/demo/controller/UserControllerIntergrationTest.java
+++ demo-project-spring-boot-master/src/test/java/com/example/demo/controller/UserControllerIntergrationTest.java
@@ -3,20 +3,19 @@ package com.example.demo.controller;
 import com.example.demo.DemoProjectApplication;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.event.annotation.AfterTestClass;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import java.text.ParseException;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,6 +31,11 @@ public class UserControllerIntergrationTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    private int port = 8089;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(port);
 
     @AfterTestClass
     public void resetDb() {
